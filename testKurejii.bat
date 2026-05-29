7992e3da535823182c4627074debd8f2df6fac24
KrazeeTobi <67659553+KrazeeTobi@users.noreply.github.com>
2026-05-29 20:23:53 +0100

    today we discover...
    how fucking bad Tobi actually is making a DOOM map. I literally made this as an idea for the credits map and it's fucking awful
    
    actually shoot me with a Comically Big Fucking Master Emerald (CBFME)

--- testKurejii.bat
+++ testKurejii.bat
@@ -4,4 +4,4 @@ del SDOOM2.pk3
 del SONIC.pk3
 copy "D:\Git Stuff\sd2plus\SDOOM2.pk3" SDOOM2.pk3
 copy "D:\Git Stuff\sd2plus\SONIC.pk3" SONIC.pk3
-uzdoom -iwad doom2.wad -file SDOOM2.pk3 SONIC.pk3
+uzdoom -iwad doom2.wad -file SDOOM2.pk3 SONIC.pk3 MAPPY.wad
